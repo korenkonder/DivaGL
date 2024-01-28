d385f53bed82f5763f25c8260bc52e66e5f62211
korenkonder <admin@mysterious-home.net>
2024-01-28 21:53:30 +0300

    More asm tests

--- src/DivaGL/common.asm
+++ src/DivaGL/common.asm
@@ -28,19 +28,49 @@ popaq MACRO
     pop rax
 ENDM
 
-EXTERN ?pv_game_load_state_13_tail_impl@@YAX_K@Z : PROC
+EXTERN ?pv_game_load_state_6_tail_impl@@YAX_K@Z : PROC
+EXTERN ?originalpv_game_load_state_6_tail@@3P6AXXZEA : QWORD
 
-.DATA
-pv_game_load_state_13_tail_jump QWORD 00000001401043ECh
+EXTERN ?pv_game_load_state_8_tail_impl@@YAX_K@Z : PROC
+EXTERN ?originalpv_game_load_state_8_tail@@3P6AXXZEA : QWORD
+
+EXTERN ?pv_game_load_state_9_mid_impl@@YA_NXZ : PROC
+EXTERN ?originalpv_game_load_state_9_mid@@3P6AXXZEA : QWORD
+
+EXTERN ?pv_game_load_state_13_tail_impl@@YAX_K@Z : PROC
+EXTERN ?originalpv_game_load_state_13_tail@@3P6AXXZEA : QWORD
 
 .CODE
+?implOfpv_game_load_state_6_tail@@YAXXZ PROC PUBLIC
+    pushaq
+    mov rcx, r14
+    call ?pv_game_load_state_6_tail_impl@@YAX_K@Z
+    popaq
+    jmp ?originalpv_game_load_state_6_tail@@3P6AXXZEA
+?implOfpv_game_load_state_6_tail@@YAXXZ ENDP
 
-PUBLIC ?implOfpv_game_load_state_13_tail@@YAXXZ
+?implOfpv_game_load_state_8_tail@@YAXXZ PROC PUBLIC
+    pushaq
+    mov rcx, r14
+    call ?pv_game_load_state_8_tail_impl@@YAX_K@Z
+    popaq
+    jmp ?originalpv_game_load_state_8_tail@@3P6AXXZEA
+?implOfpv_game_load_state_8_tail@@YAXXZ ENDP
 
-?implOfpv_game_load_state_13_tail@@YAXXZ :
+?implOfpv_game_load_state_9_mid@@YAXXZ PROC PUBLIC
+    pushaq
+    call ?pv_game_load_state_9_mid_impl@@YA_NXZ
+    test al, al
+    popaq
+    cmovne ebx,r15d
+    jmp ?originalpv_game_load_state_9_mid@@3P6AXXZEA
+?implOfpv_game_load_state_9_mid@@YAXXZ ENDP
+
+?implOfpv_game_load_state_13_tail@@YAXXZ PROC PUBLIC
     pushaq
     mov rcx, r14
     call ?pv_game_load_state_13_tail_impl@@YAX_K@Z
     popaq
-    jmp [pv_game_load_state_13_tail_jump]
+    jmp ?originalpv_game_load_state_13_tail@@3P6AXXZEA
+?implOfpv_game_load_state_13_tail@@YAXXZ ENDP
 END
--- src/DivaGL/pv_game.cpp
+++ src/DivaGL/pv_game.cpp
@@ -6,12 +6,32 @@
 #include "pv_game.hpp"
 #include <Helpers.h>
 
+void pv_game_load_state_6_tail_impl(size_t pv_game) {
+    *(uint32_t*)(pv_game + 0x08) = 7;
+}
+
+void pv_game_load_state_8_tail_impl(size_t pv_game) {
+    *(uint32_t*)(pv_game + 0x08) = 9;
+}
+
+bool pv_game_load_state_9_mid_impl() {
+    return false;
+}
+
 void pv_game_load_state_13_tail_impl(size_t pv_game) {
     *(uint32_t*)(pv_game + 0x08) = 14;
 }
 
+HOOK(void, FASTCALL, pv_game_load_state_6_tail, 0x00000001401011CE);
+HOOK(void, FASTCALL, pv_game_load_state_8_tail, 0x00000001401024E1);
+HOOK(void, FASTCALL, pv_game_load_state_9_mid, 0x000000014010280D);
 HOOK(void, FASTCALL, pv_game_load_state_13_tail, 0x000000014010386E);
 
 void pv_game_patch() {
+    INSTALL_HOOK(pv_game_load_state_6_tail);
+    INSTALL_HOOK(pv_game_load_state_8_tail);
+    INSTALL_HOOK(pv_game_load_state_9_mid);
     INSTALL_HOOK(pv_game_load_state_13_tail);
+
+    WRITE_MEMORY_STRING(0x00000001401027D8, "\x41\x0F\x45\xDF\x75\x2F", 0x06);
 }
