dda0623567d1d45ff98aa86ffc321bedc7b58257
korenkonder <admin@mysterious-home.net>
2024-03-10 10:47:01 +0300

    Fixed crash on OBJECT TEST

--- src/DivaGL/shadow.cpp
+++ src/DivaGL/shadow.cpp
@@ -8,7 +8,7 @@
 Shadow** shadow_ptr = (Shadow**)0x000000014CC587B8;
 
 void(FASTCALL* sub_1405E8A20)(Shadow* shad, int32_t index, vec3* pos)
-    = (void(FASTCALL*)(Shadow* shad, int32_t index, vec3 * pos))0x000000014063FA90;
+    = (void(FASTCALL*)(Shadow* shad, int32_t index, vec3 * pos))0x00000001405E8A20;
 
 float_t Shadow::get_shadow_range() {
     return shadow_range * shadow_range_factor;
